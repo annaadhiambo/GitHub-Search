68f5e1030e0b120b79388d30372a1371fb211ef1
annaadhiambo <annaadhiambo01@gmail.com>
2020-08-30 11:58:28 +0300

    genereted class

--- src/app/git-service/git.service.ts
+++ src/app/git-service/git.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Git } from '../git';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
+
+  getGit(){
+    return Git
+  }
 
   constructor() { }
 }
